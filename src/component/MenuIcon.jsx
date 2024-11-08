3b22946433186719653d48e194fd0fc652c4254e
rachelomo <omojoladeadebisi11@gmail.com>
2024-11-08 04:56:08 -0800

    Initial commit

--- src/component/MenuIcon.jsx
+++ src/component/MenuIcon.jsx
@@ -9,96 +9,100 @@ import AboutUs from "./AboutUs.jsx";
 import Adds from "./Adds.jsx";
 import SchoolMenu from "./SchoolMenu.jsx";
 import HouseMenu from "./HouseMenu.jsx";
-import TenantNenu from "./TenantMenu.jsx";
+import TenantMenu from "./TenantMenu.jsx";
 
 const MenuIcon = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
     setIsClicked(!isClicked);
   };
-  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <div className="MenuIcon">
-      <div className="">
-        <Link to="/">
-          <img
-            src={logo}
-            alt="logo"
-            style={{ width: "54vw", height: "10vh" }}
-          />
-        </Link>{" "}
-      </div>
-
-      <div className="flex mt-5 justify-between">
-        <div>
-          <div className="dp">
-            <Link
-              to=""
-              className=" bg-customBlue font-montserrat font-normal text-xs md:text-base rounded-md uppercase text-white p-2 pl-7 pr-7"
-            >
-              Join us
-            </Link>
-            <div className="dpc">
-              <a href="SignUp">
-                <li className="li">I am a landlord</li>
-              </a>
-
-              <Link to="/" style={{ cursor: "pointer" }}>
-                <li className="li"> I am a tenant</li>
-              </Link>
-            </div>
-          </div>
-          <span className="dropdown-account">
-            <Link to="" className="gray index">
-              {" "}
-              Login
-            </Link>
-          </span>
-        </div>
-        <div className="absolute left-80">
-        <div style={{ fontSize: "5vw", borderRadius: "5px" }}>
-  <FaBars
-    onClick={toggleMenu}
-    className={`bg-or cursor-pointer outline-none border focus:bg-customBlue ${
-      isClicked ? "bg-customBlue" : "bg-gry-500"} focus:bg-blue-500`}
-  />
+    <div className="MenuIcon flex flex-col w-full">
+  {/* Logo Section */}
+<div className=" flex justify-center mb-2 px-4">
+  <Link to="/" className="w-full">
+    <img
+      src={logo}
+      alt="logo"
+      className="w-full h-16 object-contain" // Full-width with adjusted height
+      style={{ maxWidth: "100%" }}
+    />
+  </Link>
 </div>
 
-          {menuOpen && (
-            <div
-              className="bg-w menu-overlay text-white flex flex-col pl-20 text-semibold pt-5"
-              style={{
-                width: "120vw",
-                height: "70vh",
-                maxWidth: "100%",
-                position: "relative",
-                right: "70vw",
-                top: "2vh",
-                fontSize: "3vh",
-              }}
-            >
-              <SchoolMenu />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <HouseMenu />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <TenantNenu />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <Price />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <FAQ />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <BLog />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <AboutUs />
-              <div className="border-t-2 border-gry w-full my-4"></div>
-              <Adds />
-            </div>
-          )}
+
+      {/* Join Us and Login Section */}
+      {/* flex w-full justify-around items-center */}
+    <div className="flex justify-around">
+    <div>
+      <div className="dp relative right-[15vw] md:relative md:right-0">
+    <Link
+      to=""
+      className=" bg-customBlue font-montserrat font-normal text-xs md:text-base rounded-md uppercase text-white p-2 pl-7 pr-7"
+    >
+      Join us
+    </Link>
+    <div className="dpc">
+      <a href="SignUp">
+        <li className="li">I am a landlord</li>
+      </a>
+
+      <Link to="/" style={{ cursor: "pointer" }}>
+        <li className="li"> I am a tenant</li>
+      </Link>
+    </div>
+  </div>
+  <span className="dropdown-account relative right-[14vw] md:relative md:right-0">
+    <Link to="" className="gray index">
+      {" "}
+      Login
+    </Link>
+  </span>
+    </div>
+{/* </div> */}
+        {/* Menu Toggle Button */}
+        <div className="relative right-[3vw] md:relative md:right-0">
+          <FaBars
+            onClick={toggleMenu}
+            className={`text-3xl p-2  cursor-pointer ${
+              isClicked ? "bg-customBlue text-black" : "text-black"
+            }`}
+          />
         </div>
       </div>
+
+      {/* Menu Overlay */}
+      {menuOpen && (
+        <div
+          className="menu-overlay bg-gray-800 text-white fixed inset-0 z-10 flex flex-col items-center p-6 space-y-4 overflow-y-auto"
+          style={{
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <div className="w-full max-w-md flex flex-col items-start space-y-3 text-lg font-semibold">
+            <SchoolMenu />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <HouseMenu />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <TenantMenu />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <Price />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <FAQ />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <BLog />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <AboutUs />
+            <div className="border-t border-gray-600 w-full my-2"></div>
+            <Adds />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
